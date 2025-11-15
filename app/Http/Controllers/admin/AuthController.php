f6e3872298cb9c0c9d895a69bf0fe838518a8046
haidercse <haider.cse7644@gmail.com>
2025-11-15 12:15:30 +0100

    auth

--- app/Http/Controllers/admin/AuthController.php
+++ app/Http/Controllers/admin/AuthController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
-use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -17,12 +16,10 @@ class AuthController extends Controller
 
     public function loginAll(Request $request)
     {
-       
         $request->validate([
             'email' => 'required|email',
             'password' => 'required|min:6',
         ]);
-
         $remember = $request->filled('remember');
         if (Auth::attempt($request->only('email', 'password'), $remember)) {
             $request->session()->regenerate();
